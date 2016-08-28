6322a32bf14724e49e5736f21112b7b14002bfa5
ipozdeev <igor.pozdeev@unisg.ch>
2016-08-28 21:58:32 +0200

    implied vol, loss function, price under mixture distr, more tests

--- optools.py
+++ optools.py
@@ -1,27 +1,91 @@
 #
 import numpy as np
 from scipy.special import erf
+from scipy.stats import norm
+from scipy.optimize import fsolve
 
-def priceUnderMixture(X, K, rf, tau, mu, sigma, w):
+def lossWrapper(par, wght, K, rf, cTrue, fTrue, is_iv, W):
     """
-    with mu and sigmas in row dimension, strikes in column dimension
+    """
+    # decompose par into mu and sigma
+    mu = par[1,:]
+    sigma = par[2,:]
+
+    # fitted values
+    cHat = priceUnderMixture(K, rf, mu, sigma, wght)
+    fHat = np.dot(wght, np.exp(mu + 0.5*sigma*sigma))
+
+    # if implied_vol
+    if is_iv:
+        cHat = bsIV()
+
+
+def lossFun(cTrue, cHat, fTrue, fHat, is_iv = True, W = None):
+    """
+    Loss function.
     Parameters
     ----------
-    X: float
-        price of the underlying
+    cTrue: numpy.array
+        of true call prices/ivs
+    """
+    if W is None:
+        W = np.eye(len(cHat))
+
+    cDev = cTrue - cHat
+    fDev = np.log(fTrue/fHat) if is_iv else (fTrue - fHat)
+
+    loss = 1e04*(np.dot(cDev, np.dot(W, cDev)) + fDev*fDev)
+
+    return loss
+
+def bsIV(cHat, f, K, rf, tau):
+    """
+    """
+    # fprime: derivative of bsPrice, or vega
+
+    # fprime: f*e^{-rf*tau} is the same as S*e^{-y*tau}
+    # lower part is dplus
+    fPrime = lambda x: np.diag(f*np.exp(-rf*tau)*np.sqrt(tau)*norm.pdf(
+        (np.log(f/K) + x*x/2*tau)/(x*np.sqrt(tau))))
+
+    # fPrime(sigma)
+
+    # saddle point (Wystup (2006), p. 19)
+    saddle = np.sqrt(2/tau * np.abs(np.log(f/K)))
+
+    # make sure it is positive, else set it next to 0
+    saddle *= 0.9
+    saddle[saddle <= 0] = 0.1
+
+    # solve with fsolve
+    res = fsolve(func = lambda x: bsIVobjective(cHat, f, K, rf, tau, x),
+        x0 = saddle, fprime = fPrime)
+
+    return res
+
+def bsIVobjective(cHat, f, K, rf, tau, sigma):
+    """
+    """
+    c = bsPrice(f, K, rf, tau, sigma) - cHat
+
+    return c
+
+    # return c.dot(np.eye(len(sigma)).dot(c))
+
+def priceUnderMixture(K, rf, mu, sigma, wght):
+    """
+    Computes the price of a call option under assumption that the underlying follows a mixture of lognormal distributions with parameters specified in 3mu* and *sigma* and weights specified in *w*.
+    Parameters
+    ----------
     K: numpy.array
         of strike prices
     rf: float
-        risk-free rate, in fractions of 1, annualized
-    y: float TODO: do we need this???
-        dividend yield (foreign risk-free rate), in fractions of 1, annualized
-    tau: float
-        time to expiry, in years
+        risk-free rate, in fractions of 1, per period
     mu: numpy.array
         of means of log-normal distributions in the mixture
     sigma: numpy.array
         of st. deviations of log-normal distributions in the mixture
-    w: numpy.array
+    wght: numpy.array
         of component weights
     Returns
     -------
@@ -30,30 +94,35 @@ def priceUnderMixture(X, K, rf, tau, mu, sigma, w):
     p: numpy.array
         of put option prices
     """
-    # f =
-    # c, p = bsPrice(X, K, rf, y, tau, sigma)
-    #
-    # return c.dot(w), p.dot(w)
-    pass
+    # tile with K a matrix with rows for distributional components
+    K = np.array([K,]*len(K))
 
-def blPrice(f, K, rf, tau, sigma):
+    # tile with mu and sigma matrices with columns for strikes
+    mu = np.array([mu,]*len(K)).transpose()
+    # %timeit sigma = np.tile(sigma[np.newaxis].T, (1, len(K)))
+    sigma = np.array([sigma,]*len(K)).transpose()
+
+    # calculate forward price based on distributional assumptions
+    f = np.exp(mu + 0.5*sigma*sigma)
+
+    # finally, call prices
+    c = bsPrice(f, K, rf, tau = 1, sigma = sigma)
+
+    return wght.dot(c)
+
+def bsPrice(f, K, rf, tau, sigma):
     """
-    Black-Scholes formula interms of forward price. Definitions are from Wystup (2006).
+    Black-Scholes formula in terms of forward price *f*. Definitions are from Wystup (2006).
     """
 
     # d+ and d-
     dplus = (np.log(f/K) + sigma*sigma/2*tau)/(sigma*np.sqrt(tau))
     dminus = dplus - sigma*np.sqrt(tau)
 
-    # phi = 1 for calls, -1 for puts
-    res = list()
-    for phi in [1, -1]:
-        res.append(phi*np.exp(-rf*tau)*(
-            f*fastNormCdf(phi*dplus) -
-            K*fastNormCdf(phi*dminus)))
+    res = np.exp(-rf*tau)*(f*fastNormCdf(dplus) - K*fastNormCdf(dminus))
 
     # return
-    return res[0], res[1]
+    return res
 
 def fastNormCdf(x):
     """
--- tests/test_optools.py
+++ tests/test_optools.py
@@ -8,7 +8,7 @@ import timeit
 
 from optools import optools as op
 
-class TestOptools(unittest.TestCase):
+class TestSimpleFormulas(unittest.TestCase):
     """
     """
     def setUp(self):
@@ -28,37 +28,73 @@ class TestOptools(unittest.TestCase):
         self.f = self.X*np.exp((self.rf-self.y)*self.tau)
         self.f_rn = np.exp(self.mu + 0.5*self.sigma*self.sigma)
 
+        # from a website
         self.true_call = np.array([[1.0769, 0.], [2.2353, 0.0014]])
-        self.true_put = np.array([[1.2463, 20.1355], [2.4047, 20.1369]])
 
-    def test_blPrice(self):
+    def test_bsPrice(self):
         """
         """
-        res_call, res_put = \
-            op.blPrice(self.f, self.K[1,:], self.rf, self.tau,
+        res_call = \
+            op.bsPrice(self.f, self.K[1,:], self.rf, self.tau,
                 self.sigma[1,:])
 
         assert_array_almost_equal(res_call, self.true_call[1,:], decimal = 4)
-        assert_array_almost_equal(res_put, self.true_put[1,:], decimal = 4)
 
-    def test_blPrice_on_nDim(self):
+    def test_bsPrice_on_nDim(self):
         """
+        N-dimensional inputs
         """
-        res_call, res_put = \
-            op.blPrice(self.f, self.K, self.rf, self.tau, self.sigma)
+        res_call = \
+            op.bsPrice(self.f, self.K, self.rf, self.tau, self.sigma)
 
         assert_array_almost_equal(res_call, self.true_call, decimal = 4)
-        assert_array_almost_equal(res_put, self.true_put, decimal = 4)
+
+    def test_lossFun(self):
+
+        res = op.lossFun(np.array([1,2]), np.array([2,4]),
+            1, 1, is_iv = True)
+
+        self.assertEqual(res, 5e04)
+
+    def test_bsIV(self):
+        """
+        """
+        sigma = np.random.random(2)*2
+        cHat = \
+            op.bsPrice(self.f, self.K[1,:], self.rf, self.tau, sigma)
+
+        res = op.bsIV(cHat, self.f, self.K[1,:], self.rf, self.tau)
+
+        assert_array_almost_equal(res, sigma, decimal = 4)
+
+
+class TestOptools(unittest.TestCase):
+    """
+    """
+    def setUp(self):
+        """
+        K = 100    sigma = 0.50    mu = 4.0    X = 100
+            110            0.75         5.0
+        """
+        self.X = 100
+        self.K = np.array([100, 110])
+        self.rf = 0.02*31/365
+        self.mu = np.array([4., 5.])
+        self.sigma = np.array([0.5, 0.75])
+        self.w = np.array([0.25, 0.75])
+
+        # from Paul Soederlind's function
+        self.true_call = np.array([80.7581, 75.4438])
 
     def test_priceUnderMixture(self):
         """
         """
-        res_call, res_put = \
-            op.priceUnderMixture(self.X, self.K, self.rf,
-                self.rf, self.sigma)
+        res_call = \
+            op.priceUnderMixture(self.K, self.rf, self.mu, self.sigma, self.w)
 
-        assert_array_almost_equal(res_call, self.true_call, decimal = 4)
-        assert_array_almost_equal(res_put, self.true_put, decimal = 4)
+        assert_array_almost_equal(
+            res_call, self.true_call, decimal = 4)
+
 
 if __name__ == "__main__":
     unittest.main()
