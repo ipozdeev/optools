342fd146a66b5e261b27af055bb56a03c3a38396
HSG-Spezial <HSG-Spezial>
2016-09-05 14:51:26 +0200

    halfway into estimation of rnd parameters, more comments, better handling of arrays

--- optools.py
+++ optools.py
@@ -2,44 +2,115 @@
 import numpy as np
 from scipy.special import erf
 from scipy.stats import norm
-from scipy.optimize import fsolve
+from scipy.optimize import fsolve, minimize, fmin
 
-# def cosntruct
+def estimate_rnd(cTrue, fTrue, K, rf, tau, is_iv, W, **kwargs):
+    """
+    everything is per period!
+    TODO: THIS FUNCTION IS NOW HALF-WAY DONE
+    """
+    # # weight by vegas
+    # vega = 1/(vega**vega)
+    # vega = vega/np.max(vega)
+    # W = diag(vega)
+    #
+    # # upper bound
 
-def objFunForRND(par, wght, K, rf, cTrue, fTrue, is_iv, W = None):
+    if not is_iv:
+        cTrue = bs_iv(cTrue, fTrue, K, rf, tau)
+
+    # initial values for mu and sigma
+    par0 = np.array([np.log(fTrue), np.median(cTrue)], dtype = float)
+
+    # objective function
+    obj_fun = lambda x: \
+        objective_for_rnd(x, np.array([1,]), K, rf, cTrue, fTrue, True, W)
+
+    # optimization problem
+    clever_guess = minimize(obj_fun, par0, method = "Powell", **kwargs)
+
+    return(clever_guess.x)
+
+def objective_for_rnd(par, wght, K, rf, cTrue, fTrue, is_iv, W = None):
+    """Compute objective function for minimization problem in RND estimation.
+
+    Objective function is loss function of errors between prices (IVs) of options priced under mixture of normals vs. true provided pricse (IVs).
+
+    Parameters
+    ----------
+    par: numpy.ndarray
+        [[means], [stds]] of individual components, (2,N)
+    wght: numpy.array
+        (N,) weights of each component (in frac of 1)
+    K: numpy.array
+        (M,) array of strike prices
+    rf: float
+        risk-free rate, per period (in frac of 1)
+    cTrue: numpy.array
+        (M,) array of real-world option prices (IVs)
+    fTrue: float
+        real-world price of forward contract on underlying
+    is_iv: boolean
+        True if `cTrue` are option IVs rather than prices
+    W: numpy.ndarray_like
+        weights to components of loss function
+
+    Return
+    ------
+    res: float
+        loss function value
     """
-    """
+    # number of components
+    N = len(wght)
     # decompose par into mu and sigma
-    mu = par[0,:]
-    sigma = par[1,:]
+    mu = par[:N]
+    sigma = par[N:]
 
     # fitted values
-    cHat = priceUnderMixture(K, rf, mu, sigma, wght)
+    cHat = price_under_mixture(K, rf, mu, sigma, wght)
     fHat = np.dot(wght, np.exp(mu + 0.5*sigma*sigma))
 
     # if implied_vol, transform to iv and log-prices
     if is_iv:
-        cHat = bsIV(cHat, fHat, K, rf, tau = 1)
+        # tau = 1 to avoid rescaling; make sure rf is per period!
+        cHat = bs_iv(cHat, fHat, K, rf, tau = 1)
         fHat = np.log(fHat); fTrue = np.log(fTrue)
 
     # pack into objective
-    res = lossFun(cTrue, cHat, fTrue, fHat, W)
+    res = loss_fun(cTrue, cHat, fTrue, fHat, W)
 
     return res
 
-def lossFun(cTrue, cHat, fTrue, fHat, W = None):
-    """
-    Loss function.
+def loss_fun(cTrue, cHat, fTrue, fHat, W = None):
+    """Compute value of loss function.
+
+    Quadratic loss with weights defined in `W`. Weight of forward pricing error is exactly 1, so rescale `W` accordingly.
+
     Parameters
     ----------
     cTrue: numpy.array
-        of true call prices/ivs
+        (M,) array of real-world option prices (IVs)
+    cHat: numpy.array
+        (M,) array of fitted option prices (IVs)
+    fTrue: float
+        real-world price of forward contract on underlying
+    fHat: float
+        fitted price of forward contract on underlying
+    W: numpy.ndarray_like
+        weights to components of loss function
+
+    Return
+    ------
+    loss: float
+        value of loss function
     """
+    # if no weighting matrix provided, use equal weighting
     if W is None:
         W = np.eye(len(cHat))
 
     # deviations from options prices (ivs)
     cDev = cTrue - cHat
+
     # deviations from forward price (log-price)
     fDev = fTrue - fHat
 
@@ -48,14 +119,32 @@ def lossFun(cTrue, cHat, fTrue, fHat, W = None):
 
     return loss
 
-def bsIV(cHat, f, K, rf, tau, **kwargs):
-    """
+def bs_iv(c, f, K, rf, tau, **kwargs):
+    """Compute Black-Scholes implied volatility.
+
+    Inversion of Black-Scholes formula to obtain implied volatility. Saddle point is calculated and used as initial guess for x.
+
     Parameters
     ----------
+    c: numpy.array
+        (M,) array of fitted option prices (IVs)
+    f: float
+        forward price of underlying
+    K: numpy.array
+        (M,) array of strike prices
+    rf: float
+        risk-free rate, per period (in frac of 1)
+    tau: float
+        time to maturity, in years
     **kwargs:
         other arguments to fsolve
+
+    Return
+    ------
+    res: numpy.array
+        (M,) array of implied volatilities
     """
-    # fprime: derivative of bsPrice, or vega
+    # fprime: derivative of bs_price, or vega
     # f*e^{-rf*tau} is the same as S*e^{-y*tau}
     # lower part is dplus
     fPrime = lambda x: np.diag(f*np.exp(-rf*tau)*np.sqrt(tau)*norm.pdf(
@@ -69,23 +158,27 @@ def bsIV(cHat, f, K, rf, tau, **kwargs):
     saddle[saddle <= 0] = 0.1
 
     # solve with fsolve
-    res = fsolve(func = lambda x: bsIVobjective(cHat, f, K, rf, tau, x),
+    res = fsolve(func = lambda x: bs_iv_objective(c, f, K, rf, tau, x),
         x0 = saddle, fprime = fPrime, **kwargs)
 
     return res
 
-def bsIVobjective(cHat, f, K, rf, tau, sigma):
+def bs_iv_objective(cHat, f, K, rf, tau, sigma):
+    """Compute discrepancy between calculated option price and provided `cHat`.
+
+    Parameters
+    ----------
+    same as in
     """
-    """
-    c = bsPrice(f, K, rf, tau, sigma) - cHat
+    c = bs_price(f, K, rf, tau, sigma) - cHat
 
     return c
 
     # return c.dot(np.eye(len(sigma)).dot(c))
 
-def priceUnderMixture(K, rf, mu, sigma, wght):
+def price_under_mixture(K, rf, mu, sigma, wght):
     """
-    Computes the price of a call option under assumption that the underlying follows a mixture of lognormal distributions with parameters specified in 3mu* and *sigma* and weights specified in *w*.
+    Computes the price of a call option under assumption that the underlying follows a mixture of lognormal distributions with parameters specified in `mu` and `sigma` and weights specified in `w`.
     Parameters
     ----------
     K: numpy.array
@@ -106,7 +199,7 @@ def priceUnderMixture(K, rf, mu, sigma, wght):
         of put option prices
     """
     # tile with K a matrix with rows for distributional components
-    K = np.array([K,]*len(K))
+    K = np.array([K,]*len(mu))
 
     # tile with mu and sigma matrices with columns for strikes
     mu = np.array([mu,]*len(K)).transpose()
@@ -117,11 +210,13 @@ def priceUnderMixture(K, rf, mu, sigma, wght):
     f = np.exp(mu + 0.5*sigma*sigma)
 
     # finally, call prices
-    c = bsPrice(f, K, rf, tau = 1, sigma = sigma)
+    c = bs_price(f, K, rf, tau = 1, sigma = sigma)
 
-    return wght.dot(c)
+    # result
+    res = wght.dot(c)
+    return res
 
-def bsPrice(f, K, rf, tau, sigma):
+def bs_price(f, K, rf, tau, sigma):
     """
     Black-Scholes formula in terms of forward price *f*. Definitions are from Wystup (2006).
     """
@@ -130,12 +225,39 @@ def bsPrice(f, K, rf, tau, sigma):
     dplus = (np.log(f/K) + sigma*sigma/2*tau)/(sigma*np.sqrt(tau))
     dminus = dplus - sigma*np.sqrt(tau)
 
-    res = np.exp(-rf*tau)*(f*fastNormCdf(dplus) - K*fastNormCdf(dminus))
+    res = np.exp(-rf*tau)*(f*fast_norm_cdf(dplus) - K*fast_norm_cdf(dminus))
 
     # return
     return res
 
-def fastNormCdf(x):
+# def bs_greeks(x = None, f = None, K, rf, T, t, sigma, y, is_call):
+#     """
+#     """
+#     tau = T-t            # time to maturity
+#     phi = is_call*2 - 1  # +1 for call, -1 for put
+#
+#     # if no forward was provided, calculate it
+#     if f is None:
+#         f = x*np.exp((rf-y)*tau)
+#     if x is None:
+#         x = f*np.exp((y-rf)*tau)
+#
+#     dplus = (np.log(f/K) + sigma*sigma/2*tau)/(sigma*np.sqrt(tau))
+#     dminus = dplus - sigma*np.sqrt(tau)
+#
+#     delta = phi*np.exp(-y*tau)*fast_norm_cdf(phi*dplus)
+#     gamma = no.exp(-y*tau)*norm.pdf(dplus)/(x*sigma*np.sqrt(tau))
+
+# def bs_vega(x = None, f = None, K, rf = None, y = None, tau, sigma,
+#     dplus = None):
+#     """
+#     """
+#     if dplus is None:
+#         dplus = (np.log(f/K) + sigma*sigma/2*tau)/(sigma*np.sqrt(tau)))
+#     vega = np.diag(f*np.exp(-rf*tau)*np.sqrt(tau)*norm.pdf(dplus)
+
+
+def fast_norm_cdf(x):
     """
     Using error function from scipy.special (o(1) faster)
     """
--- tests/test_optools.py
+++ tests/test_optools.py
@@ -31,27 +31,27 @@ class TestSimpleFormulas(unittest.TestCase):
         # from a website
         self.true_call = np.array([[1.0769, 0.], [2.2353, 0.0014]])
 
-    def test_bsPrice(self):
+    def test_bs_price(self):
         """
         """
         res_call = \
-            op.bsPrice(self.f, self.K[1,:], self.rf, self.tau,
+            op.bs_price(self.f, self.K[1,:], self.rf, self.tau,
                 self.sigma[1,:])
 
         assert_array_almost_equal(res_call, self.true_call[1,:], decimal = 4)
 
-    def test_bsPrice_on_nDim(self):
+    def test_bs_price_on_nDim(self):
         """
         N-dimensional inputs
         """
         res_call = \
-            op.bsPrice(self.f, self.K, self.rf, self.tau, self.sigma)
+            op.bs_price(self.f, self.K, self.rf, self.tau, self.sigma)
 
         assert_array_almost_equal(res_call, self.true_call, decimal = 4)
 
-    def test_lossFun(self):
+    def test_loss_fun(self):
 
-        res = op.lossFun(np.array([1,2]), np.array([2,4]),
+        res = op.loss_fun(np.array([1,2]), np.array([2,4]),
             1, 1)
 
         self.assertEqual(res, 5e04)
@@ -70,50 +70,94 @@ class TestHarderFormulas(unittest.TestCase):
 
         self.mu = np.array([4., 5.])
         self.sigma = np.array([0.5, 0.75])
-        self.par = np.vstack((self.mu, self.sigma))
+        self.par = np.hstack([self.mu, self.sigma])
 
         self.wght = np.array([0.25, 0.75])
         self.tau = 1
 
         self.f = np.dot(self.wght, np.exp(self.mu + 0.5*self.sigma*self.sigma))
         self.cHat = \
-            op.bsPrice(self.f, self.K, self.rf, self.tau, self.sigma)
+            op.bs_price(self.f, self.K, self.rf, self.tau, self.sigma)
 
         # from Paul Soederlind's function
         self.true_call = np.array([80.7581, 75.4438])
 
-    def test_priceUnderMixture(self):
+    def test_price_under_mixture(self):
         """
         """
         res_call = \
-            op.priceUnderMixture(self.K,self.rf,self.mu,self.sigma,self.wght)
+            op.price_under_mixture(self.K,self.rf,self.mu,self.sigma,self.wght)
 
         assert_array_almost_equal(
             res_call, self.true_call, decimal = 4)
 
-    def test_bsIV(self):
+    def test_price_under_mixture_one_component(self):
         """
-        Draw random sigmas to test if everything matches.
+        """
+        f = np.exp(self.mu[0] + 0.5*self.sigma[0]*self.sigma[0])
+        res_call = \
+            op.price_under_mixture(
+                self.K,
+                self.rf,
+                np.array([self.mu[0],]),
+                np.array([self.sigma[0],]),
+                np.array([1,]))
+        res_true = op.bs_price(f, self.K, self.rf, self.tau, self.sigma[0])
+
+        assert_array_almost_equal(res_call, res_true, decimal=4)
+
+    def test_bs_iv(self):
+        """
+        Draw random sigmas to test if they can be recovered.
         """
         sigma = np.random.random(2)*2
         self.cHat = \
-            op.bsPrice(self.f, self.K, self.rf, self.tau, sigma)
+            op.bs_price(self.f, self.K, self.rf, self.tau, sigma)
 
-        res = op.bsIV(self.cHat, self.f, self.K, self.rf, self.tau)
+        res = op.bs_iv(self.cHat, self.f, self.K, self.rf, self.tau)
 
         assert_array_almost_equal(res, sigma, decimal = 4)
 
-    def test_objFunForRND(self):
+    def test_objective_for_rnd(self):
         """
         """
         res_call = \
-            op.priceUnderMixture(self.K,self.rf,self.mu,self.sigma,self.wght)
+            op.price_under_mixture(self.K,self.rf,self.mu,self.sigma,self.wght)
 
         # omit W
-        res = op.objFunForRND(self.par, self.wght, self.K, self.rf,
+        res = op.objective_for_rnd(self.par, self.wght, self.K, self.rf,
             res_call, self.f, is_iv = False)
 
         self.assertAlmostEqual(res, 0.0, places = 2)
+
+    def test_estimate_rnd(self):
+        """
+        """
+        self.sigma = np.random.random(2)*2
+        fTrue = np.exp(self.mu[0] + 0.5*self.sigma[0]*self.sigma[0])
+        # call prices from given values of mu, sigma and weights
+        res_call = \
+            op.price_under_mixture(
+                self.K,
+                self.rf,
+                np.array([self.mu[0],]),
+                np.array([self.sigma[0],]),
+                np.array([1,]))
+
+        res = op.estimate_rnd(res_call, fTrue, self.K, self.rf, self.tau,
+            is_iv = False, W = None)
+
+        assert_array_almost_equal(
+            res, np.array([self.mu[0], self.sigma[0]]))
+
+
+        # # optimize!
+        # res = \
+        #     op.estimate_rnd(res_call, self.K, self.rf)
+        #
+        # # assert
+        # assert_array_almost_equal(self.mu, res.mu, places = 2)
+        # assert_array_almost_equal(self.sigma, res.sigma, places = 2)
 
 
 if __name__ == "__main__":
