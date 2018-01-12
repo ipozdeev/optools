452a65f2cb9bbbd6a3b0191bd5a723e2cbde8355
ipozdeev <igor.pozdeev@unisg.ch>
2018-01-12 18:53:42 +0100

    added maturity_str_to_float

--- helpers.py
+++ helpers.py
@@ -14,3 +14,24 @@ def fast_norm_cdf(x):
     res = 1/2 * (1 + erf(x / np.sqrt(2)))
 
     return res
+
+
+def maturity_str_to_float(mat):
+    """
+
+    Parameters
+    ----------
+    mat: str
+
+    Returns
+    -------
+
+    """
+    if mat.endswith('m'):
+        res = int(mat[:-1]) / 12.0
+    elif mat.endswith('y'):
+        res = float(mat[:-1])
+    else:
+        raise ValueError("Maturity string must end with 'm' or 'y'!")
+
+    return res
