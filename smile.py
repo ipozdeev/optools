fix args wrong order, more tests, delta functions

smile.py
@@ -297,7 +297,8 @@ class SABR(VolatilitySmile):
             sigma_ms_ = v_atm + c_["ms"]
 
             # call delta is d_, put delta is -d_
-            k_ms_ = strike_from_delta(np.array([d_, -d_]), tau, sigma_ms_,
+            k_ms_ = strike_from_delta(delta=np.array([d_, -d_]), tau=tau,
+                                      vola=sigma_ms_,
                                       is_call=np.array([True, False]),
                                       is_forward=is_forward,
                                       is_premiumadj=is_premiumadj,
@@ -404,10 +405,11 @@ class SABR(VolatilitySmile):
         # v_trial = bs_price(strike=k_ms, tau=tau,
         #                    vola=sabr(k_ms),
         #                    is_call=np.array([True, False] * n),
-        #                    **data_rest) \
+        #                    forward=data_rest["forward"],
+        #                    r_counter=data_rest["r_counter"]) \
         #     .reshape(-1, 2) \
         #     .sum(axis=1)
-
+        #
         # print("v_trial:\n")
         # print(v_trial)
         # print("v_tgt:\n")

strike.py
@@ -1,8 +1,7 @@
 import numpy as np
 from scipy.optimize import fsolve
 
-from optools.blackscholes import d2, d1
-from optools.helpers import fast_norm_cdf
+from optools.greeks import *
 
 
 def strike_from_delta(delta, vola, tau, is_call, spot=None, forward=None,
@@ -45,25 +44,23 @@ def strike_from_delta(delta, vola, tau, is_call, spot=None, forward=None,
     # function to calculate delta given strike and the rest
     if is_forward:
         if is_premiumadj:
-            def delta_fun(strike):
-                res_ = omega * strike / forward * \
-                    fast_norm_cdf(omega * d2(forward, strike, vola, tau))
+            def delta_fun(k_):
+                res_ = delta_premiumadj_forward(forward, k_, vola, tau,
+                                                is_call)
                 return res_
         else:
-            def delta_fun(strike):
-                res_ = omega * \
-                    fast_norm_cdf(omega * d1(forward, strike, vola, tau))
+            def delta_fun(k_):
+                res_ = delta_pips_forward(forward, k_, vola, tau, is_call)
                 return res_
     else:
         if is_premiumadj:
-            def delta_fun(strike):
-                res_ = omega * np.exp(-r_counter * tau) * strike / spot * \
-                       fast_norm_cdf(omega * d2(forward, strike, vola, tau))
+            def delta_fun(k_):
+                res_ = delta_premiumadj_spot(forward, spot, k_, vola,
+                                             r_counter, tau, is_call)
                 return res_
         else:
-            def delta_fun(strike):
-                res_ = omega * np.exp(-r_base * tau) * \
-                       fast_norm_cdf(omega * d1(forward, strike, vola, tau))
+            def delta_fun(k_):
+                res_ = delta_pips_spot(forward, k_, vola, r_base, tau, is_call)
                 return res_
 
     def obj_fun(strike):

tests/test_clark.py
@@ -3,7 +3,7 @@ import numpy as np
 from numpy.testing import assert_almost_equal
 
 from smile import SABR
-from strike import strike_from_delta
+from strike import strike_from_delta, strike_from_atm
 
 
 class TestClark(unittest.TestCase):
@@ -28,6 +28,18 @@ class TestClark(unittest.TestCase):
             0.1: {"ms": 3.806 / 100,
                   "rr": -1.359 / 100}
         }
+
+    def test_strike_from_atm(self):
+        """Strike from ATM (DNS)."""
+        k = strike_from_atm(
+            atm_def=self.delta_conventions["atm_def"],
+            is_premiumadj=self.delta_conventions["is_premiumadj"],
+            forward=self.data_rest["forward"],
+            vola=self.v_atm,
+            tau=self.tau
+        )
+        k_true = 1.3620  # ch. 3.5.4
+        self.assertAlmostEqual(k, k_true, places=4)
 
     def test_strike_from_delta(self):
         """Strike from delta."""
@@ -59,7 +71,7 @@ class TestClark(unittest.TestCase):
         clark_par = np.array([0.1743106, 0.81694072, -0.11268306])
         assert_almost_equal(sabr_par, clark_par, decimal=2)
 
-    def test_sabr_fit_to_fx_25d(self):
+    def test_sabr_fit_to_fx(self):
         """Calibration of SABR with 25-delta contracts."""
         sabr = SABR.fit_to_fx(
             tau=self.tau, v_atm=self.v_atm,